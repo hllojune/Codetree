96489ec7a4e020851ecb1de0c4253d6c26bb9cfa
junnyo2 <gy12wns200@gmail.com>
2026-07-07 22:47:14 +0900

    [Passed] 배열 채우고 출력하기 (48ms, 16MB)

--- trail1/배열 채우고 출력하기/filling-array-and-print.py
+++ trail1/배열 채우고 출력하기/filling-array-and-print.py
@@ -1,4 +1,6 @@
+# 배열을 구현하여 주어진 수를 입력받습니다.
 arr = list(input().split())
-rev = arr[::-1]
 
-print(rev[0]+rev[1]+rev[2]+rev[3]+rev[4]+rev[5]+rev[6]+rev[7]+rev[8]+rev[9])   
+# 9부터 0까지의 인덱스에 주어진 문자를 차례대로 출력합니다.
+for i in range(9, -1, -1):
+    print(arr[i], end="")
